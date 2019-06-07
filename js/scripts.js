8ad1618aa4eec5febb2a6057e567db998540103f
Na Hyung Choi <schoinh@gmail.com>
2019-06-07 12:06:14 -0700

    complete business logic, but does not save input to variables

--- js/scripts.js
+++ js/scripts.js
@@ -1,72 +1,19 @@
-var swiftTally = 0
-var pythonTally = 0
-var fsTally = 0
-var cppTally = 0
+var swiftTally = 0;
+var pythonTally = 0;
+var fsTally = 0;
+var cppTally = 0;
 
-var tally = function(q1Answer, q2Answer, q3Answer, q4Answer, q5Answer) {
-  if (q1Answer === "a1") {
-    swiftTally +=
+var tally = function(answer) {
+  if (answer === "a1") {
+    swiftTally += 1;
+  } else if (answer === "a2") {
+    pythonTally += 1;
+  } else if (answer === "a3") {
+    fsTally += 1;
+  } else if (answer === "a4") {
+    cppTally += 1;
   }
-  if (q1Answer === "a2") {
-    pythonTally +=
-  }
-  if (q1Answer === "a3") {
-    fsTally +=
-  }
-  if (q1Answer === "a4") {
-    cppTally +=
-  }
-  if (q2Answer === "a1") {
-    swiftTally +=
-  }
-  if (q2Answer === "a2") {
-    pythonTally +=
-  }
-  if (q2Answer === "a3") {
-    fsTally +=
-  }
-  if (q2Answer === "a4") {
-    cppTally +=
-  }
-  if (q3Answer === "a1") {
-    swiftTally +=
-  }
-  if (q3Answer === "a2") {
-    pythonTally +=
-  }
-  if (q3Answer === "a3") {
-    fsTally +=
-  }
-  if (q3Answer === "a4") {
-    cppTally +=
-  }
-  if (q4Answer === "a1") {
-    swiftTally +=
-  }
-  if (q4Answer === "a2") {
-    pythonTally +=
-  }
-  if (q4Answer === "a3") {
-    fsTally +=
-  }
-  if (q4Answer === "a4") {
-    cppTally +=
-  }
-  if (q5Answer === "a1") {
-    swiftTally +=
-  }
-  if (q5Answer === "a2") {
-    pythonTally +=
-  }
-  if (q5Answer === "a3") {
-    fsTally +=
-  }
-  if (q5Answer === "a4") {
-    cppTally +=
-  }
-}
-
-
+};
 
 $(function() {
   $(".swift, .python, .fs, .cpp").hide();
@@ -74,21 +21,41 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $(".swift, .python, .fs, .cpp").hide();
-
+    alert("form submitted");
     var q1Answer = $("input:radio[name=q1]:checked").val();
     var q2Answer = $("input:radio[name=q2]:checked").val();
     var q3Answer = $("input:radio[name=q3]:checked").val();
     var q4Answer = $("input:radio[name=q4]:checked").val();
     var q5Answer = $("input:radio[name=q5]:checked").val();
 
-    if (result === "swift") {
-      $(".swift").show();
-    } else if (result === "python") {
-      $(".python").show();
-    } else if (result === "fs") {
-      $(".fs").show();
-    } else if (result === "cpp") {
-      $(".cpp").show();
-    }
+    tally(q1Answer);
+    tally(q2Answer);
+    tally(q3Answer);
+    tally(q4Answer);
+    tally(q5Answer);
+
+    var highestTally = Math.max(swiftTally, pythonTally, fsTally, cppTally);
+
+    var result = function (highestTally) {
+      if (highestTally === swiftTally) {
+        $(".swift").show();
+      } else if (highestTally === pythonTally) {
+        $(".python").show();
+      } else if (highestTally === fsTally) {
+        $(".fs").show();
+      } else if (highestTally === cppTally) {
+        $(".cpp").show();
+      }
+    };
+
+    // if (result === "swift") {
+    //   $(".swift").show();
+    // } else if (result === "python") {
+    //   $(".python").show();
+    // } else if (result === "fs") {
+    //   $(".fs").show();
+    // } else if (result === "cpp") {
+    //   $(".cpp").show();
+    // }
   });
 });
