5a4f3c120165110724fe2434c7a74782fd713d36
Na Hyung Choi <schoinh@gmail.com>
2019-06-07 12:34:39 -0700

    introduce full functionality with sucessful tallying placeholder answers

--- js/scripts.js
+++ js/scripts.js
@@ -3,17 +3,6 @@ var pythonTally = 0;
 var fsTally = 0;
 var cppTally = 0;
 
-var tally = function(answer) {
-  if (answer === "a1") {
-    swiftTally += 1;
-  } else if (answer === "a2") {
-    pythonTally += 1;
-  } else if (answer === "a3") {
-    fsTally += 1;
-  } else if (answer === "a4") {
-    cppTally += 1;
-  }
-};
 
 $(function() {
   $(".swift, .python, .fs, .cpp").hide();
@@ -21,12 +10,23 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
     $(".swift, .python, .fs, .cpp").hide();
-    alert("form submitted");
     var q1Answer = $("input:radio[name=q1]:checked").val();
     var q2Answer = $("input:radio[name=q2]:checked").val();
     var q3Answer = $("input:radio[name=q3]:checked").val();
     var q4Answer = $("input:radio[name=q4]:checked").val();
     var q5Answer = $("input:radio[name=q5]:checked").val();
+
+    var tally = function(answer) {
+      if (answer === "a1") {
+        swiftTally += 1;
+      } else if (answer === "a2") {
+        pythonTally += 1;
+      } else if (answer === "a3") {
+        fsTally += 1;
+      } else if (answer === "a4") {
+        cppTally += 1;
+      }
+    };
 
     tally(q1Answer);
     tally(q2Answer);
@@ -36,16 +36,14 @@ $(function() {
 
     var highestTally = Math.max(swiftTally, pythonTally, fsTally, cppTally);
 
-    var result = function (highestTally) {
-      if (highestTally === swiftTally) {
-        $(".swift").show();
-      } else if (highestTally === pythonTally) {
-        $(".python").show();
-      } else if (highestTally === fsTally) {
-        $(".fs").show();
-      } else if (highestTally === cppTally) {
-        $(".cpp").show();
-      }
+    if (highestTally === swiftTally) {
+      $(".swift").show();
+    } else if (highestTally === pythonTally) {
+      $(".python").show();
+    } else if (highestTally === fsTally) {
+      $(".fs").show();
+    } else if (highestTally === cppTally) {
+      $(".cpp").show();
     };
 
     // if (result === "swift") {
